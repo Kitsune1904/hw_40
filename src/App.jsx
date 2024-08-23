655fa1e3c94e7b089ed0c409dfcab99f533b4c98
Kitsune1904 <annagumiho190423@gmail.com>
2024-08-23 14:10:44 +0300

    change base

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import {Notes} from "./components/Notes/Notes.jsx";
 function App() {
   return (
     <AlbumsProvider>
-      <BrowserRouter basename={"main--kitsune40.netlify.app/"}>
+      <BrowserRouter  basename={"hw_40"}>
         <Navigation></Navigation>
         <Routes>
           <Route path={'/'} element={<Main/>}></Route>
